574bff362f73de55564b7bf129ce9fb6964ee47d
HipWilson <pen24760@uvg.edu.gt>
2025-02-11 17:34:09 -0600

    Main

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        int[] numeros = GeneradorNumeros.generarNumerosAleatorios(10, 10000);
+        GeneradorNumeros.guardarEnArchivo(numeros, "numeros.txt");
+
+        int[] datos = LectorArchivo.leerDesdeArchivo("numeros.txt");
+       
+        System.out.println("Ordenando con diferentes algoritmos...");
+
+        MedidorTiempo.medir(new InsertionSort(), datos, "Insertion Sort");
+        MedidorTiempo.medir(new MergeSort(), datos, "Merge Sort");
+        MedidorTiempo.medir(new QuickSort(), datos, "Quick Sort");
+        MedidorTiempo.medir(new RadixSort(), datos, "Radix Sort");
+        MedidorTiempo.medir(new BucketSort(), datos, "Bucket Sort");
+    }
 }
--- MergeSort.java
+++ MergeSort.java
@@ -31,4 +31,4 @@ public class MergeSort implements AlgoritmosOrdenamiento {
         }
     }
 }
-}
+
